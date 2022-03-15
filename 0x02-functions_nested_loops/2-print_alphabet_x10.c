85b893aec90cc25d06f25c006f48946efd7e537f
Kiprotich Brian <thalamine@gmail.com>
2022-03-15 12:18:19 +0000

    Added 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 1;
+
+	while (i <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+	}
+	putchar('\n');
+}
