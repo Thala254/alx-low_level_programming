aaef3c1a410a5ad105ccdcbd2745ff4d1a95ef88
Kiprotich Brian <thalamine@gmail.com>
2022-03-15 14:48:06 +0000

    Corrected 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			c = a * b;
+			c = (a * b);
 			if (b != 0)
 			{
 				_putchar(',');
@@ -30,7 +30,6 @@ void times_table(void)
 				_putchar((c % 10) + '0');
 			}
 			else
-			{
 				_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
