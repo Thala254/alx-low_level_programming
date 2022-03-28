7225c8134bcbec641160e457bc4e3f7986fd0b77
thala254 <thalamine@gmail.com>
2022-03-29 02:08:25 +0300

    Added mains and crackme folders

---
+++ 0x07-pointers_arrays_strings/mains/2-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+    if (f !== NULL)
+        printf("%s\n", f);
+    return (0);
+}
+
+/*
+Write a function that locates a character in a string.
+
+    Prototype: char *_strchr(char *s, char c);
+    Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+
+FYI: The standard library provides a similar function: strchr. Run man strchr to learn more.
+
+Sample output:
+
+llo
+
+*/
