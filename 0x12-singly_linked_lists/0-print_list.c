834a5dbfac2c7eda0a6516d863131f71d98e64a0
thala254 <thalamine@gmail.com>
2022-04-22 22:37:31 +0300

    corrected 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,10 @@ size_t print_list(const list_t *h)
 	tmp = h;
 	for (i = 0; tmp; i++)
 	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
+		if (tmp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", tmp->len, tmp->str);
 		tmp = tmp->next;
 	}
 	return (i);
